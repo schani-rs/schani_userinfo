9679fb8a16175fd61821f846a9c2d0e237f0bedf
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-06-01 11:23:49 +0200

    Add API and CLI tools to add and list users
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/bin/create_user.rs
@@ -0,0 +1,20 @@
+extern crate schani_userinfo;
+extern crate diesel;
+
+use self::schani_userinfo::*;
+use std::io::stdin;
+
+fn main() {
+    let connection = establish_connection();
+
+    println!("What would you like your username and password to be?");
+    let mut username = String::new();
+    stdin().read_line(&mut username).unwrap();
+    let username = &username[..(username.len() - 1)]; // Drop the newline character
+    let mut password = String::new();
+    stdin().read_line(&mut password).unwrap();
+    let password = &password[..(password.len() - 1)]; // Drop the newline character
+
+    let user = create_user(&connection, &username.trim(), &password.trim());
+    println!("\nSaved user {} with id {}", username, user.id);
+}
---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+use self::models::{User, NewUser};
+
+pub mod schema;
+pub mod models;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn get_users<'a>(conn: &PgConnection) -> Vec<User> {
+    use schema::users::dsl::*;
+
+    users.load::<User>(conn).expect("Error loading users")
+}
+
+pub fn create_user<'a>(conn: &PgConnection, username: &'a str, password: &'a str) -> User {
+    use schema::users;
+
+    let new_user = NewUser {
+        username: username,
+        password: password,
+    };
+
+    diesel::insert(&new_user)
+        .into(users::table)
+        .get_result(conn)
+        .expect("Error saving new user")
+}
