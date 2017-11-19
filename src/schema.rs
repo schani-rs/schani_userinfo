f01d4d9b74675bff161a05125d9b55d25bed4832
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-11-19 20:10:36 +0100

    Bring repo/app back to life
    * Update to compilable Rocket
    * Fix README instructions
    * Update deps
    
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,18 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    settings (id) {
+        id -> Int4,
+        user_id -> Int4,
+        key -> Varchar,
+        value -> Varchar,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        username -> Varchar,
+        password -> Varchar,
+    }
+}
+
+joinable!(settings -> users (user_id));
